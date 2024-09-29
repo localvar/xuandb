57bffcbb5558159307053060f227518c39dbbc1d
Bomin Zhang <localvar@hotmail.com>
2024-09-29 19:49:08 +0800

    make editor happy

--- pkg/conf/conf_client.go
+++ pkg/conf/conf_client.go
@@ -1,4 +1,10 @@
-//go:build !xuandb_server
+// 'xuandb_editor' is to satisfy code editors, not used in the build process.
+// e.g. in vscode, add the following to settings.json to make the editor happy.
+//        "gopls": {
+//                "build.buildFlags": ["-tags=xuandb_editor"]
+//        }
+//
+//go:build !xuandb_server || xuandb_editor
 
 package conf
 
